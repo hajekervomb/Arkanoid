ee5a2fcd42b5b34e982fd40b3d77ebe3348656dd
arrrky <arkanesobaka@gmail.com>
2021-03-01 08:15:56 +0500

    Небольшой рефакторинг EnemySpawner. Добавил в .gitignore файлы Raider'А.

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,9 +8,9 @@ using Random = UnityEngine.Random;
 public class EnemySpawner : MonoBehaviour
 {
    [SerializeField] private GameObject enemyPrefab;
-   [SerializeField] private SpriteRenderer backgroundSprite;
    [SerializeField] public Transform defaultTarget;
 
+   private Vector3 screenBounds;
    private const float XBordersOfUnplayableArea = 120;
    private const float YBordersOfUnplayableArea = 10;
 
@@ -19,6 +19,7 @@ public class EnemySpawner : MonoBehaviour
 
    private void Start()
    {
+      screenBounds = MiscTools.GetScreenBounds();
       Invoke(nameof(SpawnEnemyInRandomLocation), timeBeforeFirstSpawn);
    }
 
@@ -31,15 +32,12 @@ public class EnemySpawner : MonoBehaviour
 
    public void SpawnEnemyInRandomLocation()
    {
-      GameObject enemyGameObject = Instantiate(enemyPrefab, GetRandomSpawnLocation(), quaternion.identity, this.gameObject.transform);
-      IEnemy enemy = enemyGameObject.GetComponent<IEnemy>();
-      enemy.Init();
+      Vector3 randomLocation = GetRandomSpawnLocation();
+      SpawnEnemy(randomLocation);
    }
 
    private Vector3 GetRandomSpawnLocation()
    {
-      Vector3 screenBounds = MiscTools.GetScreenBounds();
-
       float randomXCoord = Random.Range(-screenBounds.x + XBordersOfUnplayableArea, screenBounds.x - XBordersOfUnplayableArea);
       float randomYCoord = Random.Range(-screenBounds.y + YBordersOfUnplayableArea, screenBounds.y - YBordersOfUnplayableArea);
       
